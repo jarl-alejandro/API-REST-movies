161bbf8a8488811bce276d09398c252594bc56b3
jarl-alejandro <jarlalejor@gmail.com>
2015-09-19 15:51:14 -0500

    API REST de informacion sobre peliculas

---
+++ server.js
@@ -0,0 +1,27 @@
+'use strict'
+
+import http from 'http'
+import mongoose from 'mongoose'
+import App from './lib'
+
+const server = http.createServer()
+const port = process.env.PORT || 3000
+const database = "mongodb://localhost/movies"
+const movies = new App()
+
+server.on("request", movies.app)
+server.on("listening", onListening)
+
+mongoose.connect(database, onDBConnect)
+
+server.listen(port)
+
+function onDBConnect(err){
+  if(err) console.log(`ERROR: on connection to database, ${ err }`)
+  else
+    console.log(`Connection established to database`)
+}
+
+function onListening(){
+  console.log(`Server listening on http://localhost:${ port }`)
+}
